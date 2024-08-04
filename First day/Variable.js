babe3fad10ce6da5261868e2ffc48538f3355a04
umaj2906 <umaj212@gmail.com>
2024-08-04 18:10:57 +0530

    variable and datatypes

---
+++ First day/Variable.js
@@ -0,0 +1,19 @@
+// Variable:- A variable is simply a name of a storage location. It is like a container to store anything
+
+//A variable in javascript can be declared by var,let and const
+
+// const means a constant whose value can't changed
+// var and let, their value can be chaned after declaring their values
+
+const num = 999345278;
+console.log(num);
+//const num = 555666777 is not applicable becoz it is a constant
+//output= 999345278
+let age = 24;
+console.log(age);
+//output = 24
+age = 25;
+console.log(age);
+//output = 25
+
+console.table([num,age]);
---
+++ First day/datatype.js
@@ -0,0 +1,44 @@
+//Datatypes in JS
+// there are two types of datatypes in javascript One is primitive and second is non-primitive
+
+
+// A primitive datatype is a datatype in which we can't change their value oncee we set it.
+let a = 24;
+let b=a;
+b = 25 ;
+console.log(a);
+// In this value is not changed. this change is only seen in b
+
+// because we are not changing the value of a we are just changing the reference of b to a
+// so a is still 24 and b is 25
+
+//primitive{
+    //number 
+    a=30
+    console.log(a)
+    console.log(typeof a)
+    // string
+    Name = "uma"
+    console.log(Name)
+    console.log(typeof Name)
+    // boolean
+    istrue = true;
+    console.log(istrue)
+    console.log(typeof istrue)
+    // null
+    a=null;
+    console.log(a)
+    console.log(typeof a)
+    // it will give you null type of object and object is  non-primitive type
+    // undefined
+    a=undefined;
+    console.log(a)
+    console.log(typeof a)
+//}
+// Nonprimitive datatypes are those in which if we change it will reflect in its original value
+
+// Non-primitive{
+
+    // Array,object,function
+
+// }
